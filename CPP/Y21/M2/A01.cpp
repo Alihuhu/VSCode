a4d8c7da5b1c64a8afca72c191952aff767df1de
LiHu <alihuhu@foxmail.com>
2021-02-02 10:10:14 +0800

    21/2/2

---
+++ CPP/Y21/M2/A01.cpp
@@ -0,0 +1,30 @@
+#include <cstdio>
+#include <iostream>
+
+int count = 11;
+
+class A{
+    public:
+        static int count;
+};
+
+int A::count = 21;
+
+void fun()
+{
+    int count = 31;
+    count = 32;
+    printf("%d\n",count);
+}
+
+int main()
+{
+    ::count = 12;
+    int a,b,c;
+    
+    A::count = 22;
+    printf("%d\n",count);
+    printf("%d\n",A::count);
+    fun();
+    return 0;
+}
--- CPP/Y21/M2/aha.cpp
+++ CPP/Y21/M2/aha.cpp
@@ -1,6 +1,13 @@
 #include <cstdio>
+
 int main()
 {
+    //* yi
+    //? er
+    //! Alerts
+    //TODO 测试代码
+    
+    
     printf("hello\n");
     return 0;
 }
